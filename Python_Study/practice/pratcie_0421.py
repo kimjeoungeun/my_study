97b6588e21c8901c50bb28794c89e6efc4c13f20
jeongeun <kiseki5920@naver.com>
2023-04-21 18:02:41 +0900

    0421

--- Python_Study/practice/pratcie_0421.py
+++ Python_Study/practice/pratcie_0421.py
@@ -7,3 +7,21 @@ while idx <= len(prices):
     print("개수:", money//price)
     print("잔돈:", money%price)
     idx -= 1
+
+
+scores=[]
+n=0
+while n<5:
+    score=int(input("시험점수:"))
+    scores.appedn(score)
+    n+=1
+print(scores)
+
+
+n=1
+while n<10:
+    print("2*",n,"=",2*n)
+    n+=1 # n이 1일때부터 1씩 더하는 것을 반복
+
+while True: 
+  print("hi")
