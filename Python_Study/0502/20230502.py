5c57e364b73fbf8696e7797e18dfca5f263ae50a
jjni <kiseki5920@naver.com>
2023-05-04 01:09:22 +0900

    0504

--- Python_Study/0502/20230502.py
+++ Python_Study/0502/20230502.py
@@ -1,14 +1,26 @@
-import numpy as np
+# import numpy as np
 
-n=1000000
-numpy_arr = np.arange(n)
-python_list = list(range(n))
+# n=1000000
+# numpy_arr = np.arange(n)
+# python_list = list(range(n))
 
-from time import time
-python_list = [x**3+10 for x in python_list]
-time()
-import time
-start=time.time() #시작시간 저장
-python_list=[x**3+10 for x in python_list]
-# 현재시각-시작시간=실행시간
-print('time:'.time.time()-start)
+# from time import time
+# python_list = [x**3+10 for x in python_list]
+# time()
+# import time
+# start=time.time() #시작시간 저장
+# python_list=[x**3+10 for x in python_list]
+# # 현재시각-시작시간=실행시간
+# print('time:'.time.time()-start)
+
+# li_9=[5,3,1,4,2]
+# li_9.reverse()
+# print(li_9)
+
+li_8=[5,3,1,4,2]
+li_8.sort()
+print(li_8)
+
+# li_8=[5,3,1,4,2]
+# li_8.sort(reverse=True) #revers()와 서로 다름
+# print(li_8)
