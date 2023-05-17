f5e4bfa1f26409f8dce3b78322e8f09e7173b1be
jeongeun <kiseki5920@naver.com>
2023-05-17 18:09:18 +0900

    0517

--- Python_Study/practice/programus.py
+++ Python_Study/practice/programus.py
@@ -1,8 +1,11 @@
-
-a =     1
-num_list=[2, 3, 4, 5]
-# for i in range(len(num_list)-1):
-#     print(num_list[i]*num_list[i+1])
-for i in num_list:
-    a *= i
-print(a)
+def solution(dot):
+    answer = 0
+    if dot[0]>=0 and dot[1]>=0:
+        answer=1
+    elif dot[0]<0 and dot[1]<=0:
+        answer=2
+    elif dot[0]<0 and dot[1]<0:
+        answer=3
+    else
+        answer=4
+    return answer
