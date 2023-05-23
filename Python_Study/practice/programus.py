70baf03e5077e9edbb7e672dc554143a0ec45d7b
jjni <130967351+kje0058@users.noreply.github.com>
2023-05-23 12:33:24 +0900

    0523

--- Python_Study/practice/programus.py
+++ Python_Study/practice/programus.py
@@ -1,11 +1,16 @@
-def solution(dot):
-    answer = 0
-    if dot[0]>=0 and dot[1]>=0:
-        answer=1
-    elif dot[0]<0 and dot[1]<=0:
-        answer=2
-    elif dot[0]<0 and dot[1]<0:
-        answer=3
-    else
-        answer=4
-    return answer
+# def solution(dot):
+#     answer = 0
+#     if dot[0]>=0 and dot[1]>=0:
+#         answer=1
+#     elif dot[0]<0 and dot[1]<=0:
+#         answer=2
+#     elif dot[0]<0 and dot[1]<0:
+#         answer=3
+#     else
+#         answer=4
+#     return answer
+result=10
+n_plus_1(3)
+def n_plus_1(n):
+    result=n+1
+print(result)
